ea54fe904544f03780b7ff4e286c6e2fe031bf48
RadHatter <aar.j.smith@gmail.com>
2019-02-14 14:38:42 -0600

    Initial commit

---
+++ Assets/Scripts/Wizard.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Wizard : MonoBehaviour
+{
+    private int min;
+    private int max;
+    private int currentGuess;
+    private List<int> previousGuesses;
+
+    [SerializeField] private TextMeshProUGUI guessText;
+
+    void Start()
+    {
+        min = 1;
+        max = 1001;
+        currentGuess = min;
+        previousGuesses = new List<int>();
+        NextGuess();
+    }
+
+    public void NextGuess()
+    {
+        do
+        {
+            currentGuess = Random.Range(min, max + 1);
+        } while (IsPreviouslyGuessed(currentGuess) == true);
+
+        previousGuesses.Add(currentGuess);
+        guessText.text = currentGuess.ToString();        
+    }
+
+    private bool IsPreviouslyGuessed(int nextGuess)
+    {
+        bool isGuessed = false;
+        foreach(int guess in previousGuesses)
+        {
+            if (guess == nextGuess)
+            {
+                isGuessed = true;
+            }
+        }
+        return isGuessed;
+    }
+    
+    public void IsHigher()
+    {
+        min = currentGuess;
+        NextGuess();
+    }
+
+    public void IsLower()
+    {
+        max = currentGuess;
+        NextGuess();
+    }
+}
